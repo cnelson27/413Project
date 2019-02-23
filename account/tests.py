ffcdd5396f9fdef4755137647345dcd91a2d0d8b
Colby Nelson <cnelson27@outlook.com>
2019-02-22 21:46:21 -0700

    Added unit tests and got rid of timezone

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from account import models as amod
 
 # Create your tests here.
+class AccountTests(TestCase):
+    fixtures = [ 'account.yaml' ]
+
+    def test_user_get(self):
+        u2 = amod.User.objects.get(id=2)
+        # BAD BAD WOLF - just while developing this test
+        print('>>>>', u2.first_name)
+
