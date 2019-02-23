1c015b7680179e4516e0eb202ebc30f40409dcba
Colby Nelson <cnelson27@outlook.com>
2019-02-22 22:41:42 -0700

    Added logout and Account Tests

--- account/tests.py
+++ account/tests.py
@@ -1,20 +1,43 @@
 from django.test import TestCase
 from account import models as amod
+from datetime import datetime
+from lxml import etree
 
 # Create your tests here.
 class AccountTests(TestCase):
     fixtures = [ 'account.yaml' ]
 
+    def setUp(self):
+        self.homer = amod.User.objects.get(username='homer')
+        self.homer.set_password('doh!')
+        self.homer.save()
+
     def test_user_get(self):
         u2 = amod.User.objects.get(id=2)
+        u2.set_password('doh!')
         self.assertEqual(u2.first_name, 'Homer', 'Name should have been Homer')
 
-    def test_user_create(self):
-        u = amod.User()
-        u.first_name = 'Harry'
-        u.last_name = 'Potter'
-        u.username = 'harry'
-        u.set_password = 'hermione'
-        u.save()
-        u2 = amod.User.objects.get(u.username)
-        self.assertEqual(u.first_name, u2.first_name, msg='User is not created successfully')
+    def test_user_login(self):
+        credentials = {
+            'username': 'homer',
+            'password': 'doh!'
+        }
+        response = self.client.post('/account/login/', credentials)
+        # get the request object (testing framework embeds it as response.wsgi_request)
+        request = response.wsgi_request
+        # this next line is ONLY for debugging the test - it should be removed after things work
+        # self.print_html(response.content)
+        # if it worked, then request.user will be the homer object and is_authenticated will be true
+        self.assertTrue(request.user.is_authenticated, msg="User should have authenticated")
+        self.assertEqual(request.user.id, self.homer.id, msg="User should have been homer")
+        # if it worked, the response should be a redirect code (login.py returned HttpResponseRedirect)
+        self.assertEqual(response.status_code, 302, msg="User wasn't redirected")
+
+    def print_html(self, content):
+        '''Helper to pretty-print HTML'''
+        content = content.strip()
+        if content:
+            html = etree.HTML(content)
+            print(etree.tostring(html, pretty_print=True, encoding=str))
+        else:
+            print('<empty content>')
---
+++ account/views/logout.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
+from django_mako_plus import view_function
+
+@view_function
+def process_request(request):
+    logout(request)
+    return request.dmp.render('/account/login/')
+
+    
