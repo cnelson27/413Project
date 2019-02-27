7d384a28fc487db1865ba09f7b75fab7bbe1fc50
Colby Nelson <cnelson27@outlook.com>
2019-02-26 21:19:27 -0700

    Added Product model - status not fixed yet

--- catalog/models.py
+++ catalog/models.py
@@ -5,3 +5,17 @@ from django.db import models
 class Category(models.Model):
     last_modified = models.DateTimeField(auto_now=True)
     name = models.TextField()
+
+class Product(models.Model):
+    category = Category(models.ForeignKey)
+    last_modified = models.DateTimeField(auto_now=True)
+    status = models.TextField(db_index=True, choices='Active' or 'Inactive' default='Active')
+    name = models.TextField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2
+    #quantity refers to current in stock
+    quantity = models.IntegerField()
+    # reorder_trigger refers to when we reorder
+    reorder_trigger = models.IntegerField()
+    # reorder_quantity refers to how many we order
+    reorder_quantity = models.IntegerField()
