d3225857f8fb629ad63c71983526ab52373d448c
Colby Nelson <cnelson27@outlook.com>
2019-03-11 13:53:13 -0600

    Added index.py

--- catalog/views/index.py
+++ catalog/views/index.py
@@ -2,31 +2,27 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from django_mako_plus import view_function, jscontext
 from django import forms
+import math
 from catalog import models as cmod
 from django.core.exceptions import ValidationError
 
+ITEMS_PER_PAGE = 8
 
 @view_function
-def process_request(request):
-    # productnum = 0
-    # imgnum = 0
-    # for i in range(6):
-    #     prod = cmod.Product()
-    #     prod.name = PRODUCTS[productnum]
-    #     prod.status = 'A'
-    #     prod.description = 'The best ' + PRODUCTS[productnum] + ' on the market!'
-    #     prod.price = 800.85
-    #     prod.category = cmod.Category.objects.get(id = 3)
-    #     prod.save()
-    #     for j in range(2):
-    #         prodimg = cmod.ProductImage()
-    #         prodimg.filename = IMAGES[imgnum]
-    #         prodimg.product = cmod.Product.objects.get(name=PRODUCTS[productnum])
-    #         imgnum+=1
-    #         prodimg.save()
-    #     productnum+=1
+def process_request(request, category:cmod.Category=None, page:int=1):
+    products = cmod.Product.objects.filter(status="A")
+    print('>>>>>>>>>>', products)
+    if category is not None:
+        products = products.filter(category=category)
+    products = products[(page - 1) * ITEMS_PER_PAGE: page * ITEMS_PER_PAGE]
 
-    return request.dmp.render('index.html')
+    print('>>>>>>>>>>', products)
+    return request.dmp.render('index.html', {
+        'category' : category,
+        'products' : products,
+        'page' : page,
+        'numpages' : math.ceil(products.count() / ITEMS_PER_PAGE),   
+    })
 
 
     
